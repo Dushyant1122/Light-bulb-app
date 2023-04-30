4810ea4e8a9354c8ef3ded36da3240fc9694a088
Dushyant Jangid <107930946+Dushyant1122@users.noreply.github.com>
2023-04-30 11:58:08 +0530

    30-04-2023

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-   const tubelight = () =>{
-       let bulbOn = document.getElementById('bulbdata');            
-       let bulbMain = document.getElementById('bulbMain');            
-       
-       for (let x = 1; x <= 6; x++){
-           let bulb = document.getElementById('lightId'.concat(x));
-           
-                           if(bulb.src.match('lightoff')){
-                               bulb.src = "lighton.png";
-                               bulbOn.innerHTML = "Light ON";
-                               bulbOn.style.color = "red";         
-                               bulbMain.style.background = "black";
-                            }else{
-                                bulb.src = "lightoff.png";
-                                bulbOn.innerHTML = "Light OFF"
-                                bulbMain.style.background = "white";         
-                                bulbOn.style.color = "black";         
-                           }
-           
-       }
-
-        }
+var content = document.getElementsByTagName("body")[0];
+var darkMode = document.getElementById("dark-change");
+darkMode.addEventListener("click", function () {
+  darkMode.classList.toggle("active");
+  content.classList.toggle("night");
+});
